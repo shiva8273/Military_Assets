5bcaaf62006e587a6b439669f5af4dea5f8956cc
shiva8273 <shivasatish194@gmail.com>
2026-08-13 14:38:27 +0530

    equipment updated

--- Assets/views.py
+++ Assets/views.py
@@ -2,34 +2,45 @@ from django.shortcuts import render
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from .models import EquipmentType
 
 from .serializer import EquipmentTypeSerializer
 from accounts.permissions import IsAdmin
+
 # Create your views here.
+
 
 class EquipmentTypeViewSet(APIView):
     permission_classes = [IsAdmin]
-    
-    def post(self,request):
-        
-        serializer = EquipmentTypeSerializer(data = request.data)
-        
+
+    def post(self, request):
+
+        serializer = EquipmentTypeSerializer(data=request.data)
+
         if serializer.is_valid():
-            
+
             equipment_type = serializer.save()
-            
+
             return Response(
                 {
-                    "message":"Created Successfully",
-                     "equipment": {
+                    "message": "Created Successfully",
+                    "equipment": {
                         "id": equipment_type.id,
                         "name": equipment_type.name,
                         "category": equipment_type.category,
                     },
-                },status=status.HTTP_201_CREATED
+                },
+                status=status.HTTP_201_CREATED,
             )
-        return Response(
-            serializer.errors,
-            status=status.HTTP_400_BAD_REQUEST
-        )
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+    def get(self, request):
+        equipment_types = EquipmentType.objects.all()
+        serializer = EquipmentTypeSerializer(equipment_types, many=True)
+        return Response(
+            {
+                "message": "Equipment types fetched successfully",
+                "equipment": serializer.data,
+            },
+            status=status.HTTP_200_OK,
+        )
